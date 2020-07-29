5873eec22301ba6c35bfa57167acd0a8f226d7b2
hwookim <gusdn9754@gmail.com>
2020-07-29 19:43:11 +0900

    cors chore: 기술 개발을 위한 크로스 오리진 뚫기

--- src/main/java/com/grasshouse/dorandoran/post/controller/PostController.java
+++ src/main/java/com/grasshouse/dorandoran/post/controller/PostController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"https://woowacourse.com"})
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/posts")
 public class PostController {
